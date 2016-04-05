8b01fd2dd6e56e98161ad7cf391c9d85c486c0c9
Andrew Blakely <andrew.blakely@ymail.com>
2016-04-05 18:30:43 -0500

    Added more documnetation to ImmutableStatsContainer

--- src/Booma.Stats.Common/StatContainers/ImmutableStatsContainer.cs
+++ src/Booma.Stats.Common/StatContainers/ImmutableStatsContainer.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace Booma.Stats.Common
 {
+	/// <summary>
+	/// Base-class type for Immutable stats container types.
+	/// </summary>
+	/// <typeparam name="TStatType"></typeparam>
 	public abstract class ImmutableStatsContainer<TStatType> : IStatsContainer<TStatType>
 		where TStatType : struct, IConvertible
 	{
@@ -19,7 +23,15 @@ namespace Booma.Stats.Common
 		protected IEnumerable<int?> statsMap
 		{
 			get { return _statsMap; }
-		} 
+		}
+
+		/// <summary>
+		/// Creates a partially initialized immutable container for stats.
+		/// </summary>
+		public ImmutableStatsContainer(IEnumerable<StatPair<TStatType>> valuePairs)
+		{
+			
+		}
 
 		/// <summary>
 		/// Creates an empty-immutable container for stats.
@@ -31,12 +43,24 @@ namespace Booma.Stats.Common
 		}
 
 		//the reason we don't implement this generically is because this would cause boxing due to the inability to simply cast from enum to int
+		/// <summary>
+		/// Readonly index accessor that takes in <typeparamref name="TStateType"/>s
+		/// and returns the contained value. Does not throw if the container doesn't contain
+		/// <paramref name="statIndex"/>; returns null instead.
+		/// </summary>
+		/// <param name="statIndex">The state type to query the container for.</param>
+		/// <returns>The corresponding value for the <paramref name="statIndex"/> or null if the container doesn't contain it.</returns>
 		public abstract int? this[TStatType statIndex]
 		{
 			get;
 		}
 
 		//the reason we don't implement this generically is because this would cause boxing due to the inability to simply cast from enum to int
+		/// <summary>
+		/// Indicates if the container has a value for the given <paramref name="statType"/>
+		/// </summary>
+		/// <param name="statType">The <typeparamref name="TStatType"/> to check the contained status of.</param>
+		/// <returns>True if the stat is in the container.</returns>
 		public abstract bool Contains(TStatType statType);
 	}
 }
