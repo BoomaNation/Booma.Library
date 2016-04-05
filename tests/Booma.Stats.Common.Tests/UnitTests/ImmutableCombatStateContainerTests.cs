595c10f5165d2a6442e4ba27c2d18dd5800ebf04
Andrew Blakely <andrew.blakely@ymail.com>
2016-04-05 14:09:43 -0500

    Added Test Csproj

---
+++ tests/Booma.Stats.Common.Tests/UnitTests/ImmutableCombatStateContainerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Stats.Common.Tests.UnitTests
+{
+	[TestFixture]
+	public class ImmutableCombatStateContainerTests
+	{
+		public static void Ctor_Doesnt_throw()
+		{
+			//assert: doesn't throw
+			Assert.DoesNotThrow(() => new ImmutableCombatStatsContainer());
+		}
+	}
+}
