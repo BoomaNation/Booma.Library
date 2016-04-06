93e979d8d81b64c733cba0809505ba42beeea75f
Andrew Blakely <andrew.blakely@ymail.com>
2016-04-05 21:27:15 -0500

    Added ResistMultiplier strat interface

---
+++ src/Booma.Stats.Common/Formulas/IResistanceMultiplierStrategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Stats.Common
+{
+	/// <summary>
+	/// Generates multiplier values for resistances.
+	/// </summary>
+	public interface IResistanceMultiplierStrategy
+	{
+		/// <summary>
+		/// Generates a resistance multiplier for <paramref name="resistType"/>.
+		/// </summary>
+		/// <param name="resistType">Type of resistance.</param>
+		/// <returns>Float values (1.0 if no resist) that acts as a multiplier for resistance.</returns>
+		float ResistanceMultiplier(ResistanceStatType resistType);
+	}
+}
