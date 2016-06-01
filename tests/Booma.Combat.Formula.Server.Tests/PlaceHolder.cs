7e2e1792f6bfa912bff8120a79c09c6feeb5121c
Andrew Blakely <andrew.blakely@ymail.com>
2016-05-31 20:37:52 -0500

    Add Placeholder test to satisfy TravisCI

---
+++ tests/Booma.Combat.Formula.Server.Tests/PlaceHolder.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booma.Combat.Formula.Server.Tests
+{
+	[TestFixture]
+	public class PlaceHolder
+	{
+		[Test]
+		public void Empty()
+		{
+
+		}
+	}
+}
