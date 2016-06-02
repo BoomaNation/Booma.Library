f6edf7d9605de67076cb5aa98bf72c19aadb4f85
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-01 22:51:20 -0500

    Changed AdditiveStatProvider to StatProvider
    This is now basically a stat provider that indicates the units or stat
    type of the value.

--- src/Booma.Stats.Common/Formulas/IStatProvider.cs
+++ src/Booma.Stats.Common/Formulas/IStatProvider.cs
@@ -5,20 +5,22 @@ using System.Text;
 
 namespace Booma.Stats.Common
 {
+	//It's like a value that comes with a unit. The unit being the TStatType and the StatType.
 	/// <summary>
 	/// Interface for objects that can produce values that have units.
 	/// These units are indicated by both the generic parameter and the <see cref="StatType"/> property.
 	/// </summary>
-	public interface IAdditiveStatProvider<TStatType>
+	public interface IStatProvider<TStatType>
 		where TStatType : struct, IConvertible
 	{
+			
 		/// <summary>
 		/// Indicates the unit of the <see cref="Value"/> associated with this stat provider.
 		/// </summary>
 		TStatType StatType { get; }
 
 		/// <summary>
-		/// Integer value associated with the additive provider.
+		/// Integer value associated with the stat provider.
 		/// </summary>
 		int Value { get; }
 	}
