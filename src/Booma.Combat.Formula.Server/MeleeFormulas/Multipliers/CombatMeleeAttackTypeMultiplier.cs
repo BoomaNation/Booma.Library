85cec989fdd5e00b87f49c29464cec52c0daf5cb
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-03 18:05:02 -0500

    Add CombatMultplier psuedo-enum

---
+++ src/Booma.Combat.Formula.Server/MeleeFormulas/Multipliers/CombatMeleeAttackTypeMultiplier.cs
@@ -0,0 +1,36 @@
+﻿using Booma.Stats.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Combat.Formula.Server
+{
+	//Values from http://www.freewebs.com/azurepso/psostatistics.htm
+	/// <summary>
+	/// Psuedo-float-enum class that provides constant float values for mulitpliers on damage.
+	/// </summary>
+	public static class CombatMeleeAttackTypeMultiplier
+	{
+		protected class CombatAttackTypeMultiplier : IMultiplierProvider
+		{
+			public float Multiplier { get; }
+
+			public CombatAttackTypeMultiplier(float val)
+			{
+				Multiplier = val;
+			}
+		}
+
+		/// <summary>
+		/// Base/weak mutliplier for damage.
+		/// </summary>
+		public static IMultiplierProvider Base { get; } = new CombatAttackTypeMultiplier(0.9f);
+
+
+		/// <summary>
+		/// Heavy multiplier for damage.
+		/// </summary>
+		public static IMultiplierProvider Heavy { get; } = new CombatAttackTypeMultiplier(1.701f);
+	}
+}
