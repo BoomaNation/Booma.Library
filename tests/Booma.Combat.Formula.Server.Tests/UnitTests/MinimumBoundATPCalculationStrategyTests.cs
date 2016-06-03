ca0135c49a1c3913294676795c8546aac02bb76c
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-03 16:06:44 -0500

    Add MinimumBoundATPCalculation and Tests

---
+++ tests/Booma.Combat.Formula.Server.Tests/UnitTests/MinimumBoundATPCalculationStrategyTests.cs
@@ -0,0 +1,42 @@
+﻿using Booma.Stats.Common;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Combat.Formula.Server.Tests
+{
+	[TestFixture]
+	public static class MinimumBoundATPCalculationStrategyTests
+	{
+		[Test]
+		public static void Ctor_Throws_On_InvalidStatType_First_Arg()
+		{
+			//arrange
+			Mock<IStatProvider<CombatStatType>> providerWrong = new Mock<IStatProvider<CombatStatType>>(MockBehavior.Loose);
+			providerWrong.SetupGet(p => p.StatType).Returns(CombatStatType.MentalStrength);
+
+			Mock<IStatProvider<CombatStatType>> providerRight = new Mock<IStatProvider<CombatStatType>>(MockBehavior.Loose);
+			providerRight.SetupGet(p => p.StatType).Returns(CombatStatType.AttackPower);
+
+			//assert
+			Assert.Throws<ArgumentException>(() => new MinimumBoundATPCalculationStrategy(providerWrong.Object, providerRight.Object));
+		}
+
+		[Test]
+		public static void Ctor_Throws_On_InvalidStatType_Second_Arg()
+		{
+			//arrange
+			Mock<IStatProvider<CombatStatType>> providerWrong = new Mock<IStatProvider<CombatStatType>>(MockBehavior.Loose);
+			providerWrong.SetupGet(p => p.StatType).Returns(CombatStatType.MentalStrength);
+
+			Mock<IStatProvider<CombatStatType>> providerRight = new Mock<IStatProvider<CombatStatType>>(MockBehavior.Loose);
+			providerRight.SetupGet(p => p.StatType).Returns(CombatStatType.AttackPower);
+
+			//assert
+			Assert.Throws<ArgumentException>(() => new MinimumBoundATPCalculationStrategy(providerRight.Object, providerWrong.Object));
+		}
+	}
+}
