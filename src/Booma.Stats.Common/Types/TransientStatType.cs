354c43bf30ce3334b2d1baf03215f46bcf5a65e5
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-04 16:40:35 -0500

    Added Transient Stat Type
    I thought about it, it would be a pain to manage 3 full stat containers
    for base combat, base resist and current transient things like HP and
    TP.
    
    Plus it'd be a bit confusing I think if everything shared the exact same
    interface.

---
+++ src/Booma.Stats.Common/Types/TransientStatType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Stats.Common.Types
+{
+	/// <summary>
+	/// Enumeration of all potential transient stats.
+	/// These are stats that change often and are transient in the sense that they aren't saved.
+	/// </summary>
+	public enum TransientStatType
+	{
+		/// <summary>
+		/// HP - Hit Points
+		/// </summary>
+		HitPoints,
+
+		/// <summary>
+		/// TP - Technique Points
+		/// </summary>
+		TechniquePoints,
+
+		/// <summary>
+		/// PB - Photon Blast
+		/// </summary>
+		PhotonBlastCharge,
+	}
+}
