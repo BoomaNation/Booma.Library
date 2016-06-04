61cc408805645d7e1952582e447c04f5f47b302b
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-04 16:49:45 -0500

    Add Transient ToKey Extension

---
+++ src/Booma.Stats.Common/Extensions/TransientStatTypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Stats.Common
+{
+	public static class TransientStatTypeExtensions
+	{
+		public static int ToKey(this TransientStatType stat)
+		{
+			return (int)stat;
+		}
+	}
+}
