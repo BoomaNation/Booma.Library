e0fb8e84028368b505539d6afa1183b93451c3b1
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-05 00:09:38 -0500

    Added more generic ImmutableStatsContainBuilder Tests

--- tests/Booma.Stats.Common.Tests/UnitTests/StatContainers/Builder/ImmutableStatContainerBuilderTestsGeneric.cs
+++ tests/Booma.Stats.Common.Tests/UnitTests/StatContainers/Builder/ImmutableStatContainerBuilderTestsGeneric.cs
@@ -34,6 +34,58 @@ namespace Booma.Stats.Common
 			}
 		}
 
+		[Test]
+		[TestCase(-1)]
+		[TestCase(1)]
+		[TestCase(0)]
+		[TestCase(int.MaxValue)]
+		[TestCase(int.MinValue)]
+		public void Test_Builder_Inits_To_Zero_With_AllSame(int value)
+		{
+			//arrange
+			ImmutableStatsContainer<TStatType> container = BuildForGeneric(ImmutableStatsContainerBuilder<TStatType>.Factory.Create().WithAllValuesAs(value));
+
+			foreach (TStatType statType in Enum.GetValues(typeof(TStatType)))
+			{
+				//assert: That all values are present and zero
+				IStatProvider<TStatType> provider = container[statType];
+
+				Assert.NotNull(provider);
+				Assert.AreEqual(value, provider.Value);
+			}
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(1)]
+		[TestCase(0)]
+		[TestCase(int.MaxValue)]
+		[TestCase(int.MinValue)]
+		public void Test_Builder_Inits_To_Zero_With_WithStat(int value)
+		{
+			//arrange
+			var builder = ImmutableStatsContainerBuilder<TStatType>.Factory.Create();
+			ImmutableStatsContainer<TStatType> container = null;
+
+			//act
+			foreach (TStatType statType in Enum.GetValues(typeof(TStatType)))
+			{
+				builder.WithStat(statType, value);	
+			}
+
+
+			container = BuildForGeneric(builder);
+
+			foreach (TStatType statType in Enum.GetValues(typeof(TStatType)))
+			{
+				//assert: That all values are present and zero
+				IStatProvider<TStatType> provider = container[statType];
+
+				Assert.NotNull(provider);
+				Assert.AreEqual(value, provider.Value);
+			}
+		}
+
 		protected abstract ImmutableStatsContainer<TStatType> BuildForGeneric(ImmutableStatsContainerBuilder<TStatType>.IBuilder builder);
 	}
 }
