8c4fb8c98df5892edd758aef5da7f88aa15e302a
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-05 02:17:04 -0500

    Added basic Entity Interfaces

---
+++ src/Booma.Entity.Server/CombatEntities/IStatsQueryable.cs
@@ -0,0 +1,24 @@
+﻿using Booma.Stats.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Entity.Server
+{
+	/// <summary>
+	/// Interface that contracts implementers to expose stat information
+	/// </summary>
+	public interface IStatsQueryable
+	{
+		/// <summary>
+		/// Immutable container for entity's combat stats.
+		/// </summary>
+		ImmutableStatsContainer<CombatStatType> CombatStats { get; }
+
+		/// <summary>
+		/// Immutable container for entity's resistance stats.
+		/// </summary>
+		ImmutableStatsContainer<ResistanceStatType> ResistanceStats { get; }
+	}
+}
