d7f3e908ee8e3666a114640993c86f2790582918
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-04 20:48:00 -0500

    Created Extensions for ContainerBuilders

---
+++ src/Booma.Stats.Common/Extensions/StatContainerBuilder/ImmutableStatsContainerBuilderCarryObjectExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Stats.Common
+{
+	public static class ImmutableStatsContainerBuilderCarryObjectExtensions
+	{
+		//generic extensions
+		public static ImmutableStatsContainerBuilder<TStatType>.IBuilder WithStat<TStatType>(this ImmutableStatsContainerBuilder<TStatType>.IBuilder builder, TStatType statType, int statValue)
+			where TStatType : struct, IConvertible
+		{
+			//Pretty simple, just initialize the desired stat value.
+			builder[statType] = statValue;
+
+			//fluent return
+			return builder;
+		}
+
+		public static ImmutableStatsContainerBuilder<TStatType>.IBuilder WithDefaults<TStatType>(this ImmutableStatsContainerBuilder<TStatType>.IBuilder builder)
+			where TStatType : struct, IConvertible
+		{
+			return builder.WithAllValuesAs(0);
+		}
+
+		public static ImmutableStatsContainerBuilder<TStatType>.IBuilder WithAllValuesAs<TStatType>(this ImmutableStatsContainerBuilder<TStatType>.IBuilder builder, int statValue)
+			where TStatType : struct, IConvertible
+		{
+			//This is ok in Unity. Don't worry about GC.
+			foreach (TStatType stat in Enum.GetValues(typeof(TStatType)))
+			{
+				builder[stat] = statValue;
+			}
+
+			//fluent return
+			return builder;
+		}
+
+		//ResistStatType Extensions
+		public static ImmutableStatsContainerBuilder<ResistanceStatType>.IBuilder WithFireResist(this ImmutableStatsContainerBuilder<ResistanceStatType>.IBuilder builder, int resistValue)
+		{
+			builder[ResistanceStatType.ElementalFire] = resistValue;
+
+			//fluent return
+			return builder;
+		}
+
+		//Build methods
+		public static ImmutableStatsContainer<ResistanceStatType> Build(this ImmutableStatsContainerBuilder<ResistanceStatType>.IBuilder builder)
+		{
+			//create a new resistance container
+			return new ImmutableResistanceStatsContainer(builder.BuilderDictionary);
+		}
+	}
+}
