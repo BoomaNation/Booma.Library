560b4a340ec4d5f4ad1495094855b87635dc692c
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-17 23:57:48 -0500

    Simple LoginNetworkClient created with Events

---
+++ src/Booma.Client.ServerSelection.Authentication/Networking/LoginNetworkClient.cs
@@ -0,0 +1,46 @@
+﻿using Booma.Client.Network.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet.Common;
+using UnityEngine.Events;
+using UnityEngine;
+
+namespace Booma.Client.ServerSelection.Authentication
+{
+	/// <summary>
+	/// The login network client for the Boom project
+	/// </summary>
+	public class LoginNetworkClient : BoomaNetworkClientPeer<LoginNetworkClient>
+	{
+		/// <summary>
+		/// Called when the peer enters a connected state
+		/// </summary>
+		[Tooltip("Called when the peer enters a connected state.")]
+		[SerializeField]
+		private NetworkStatusUnityEvent OnConnectionEstablished;
+
+		/// <summary>
+		/// Called when the peer successfully establishes a secure channel.
+		/// </summary>
+		[Tooltip("Called when the peer successfully establishes a secure channel.")]
+		[SerializeField]
+		private NetworkStatusUnityEvent OnEncryptionEstablished;
+
+		public override void OnStatusChanged(NetStatus status)
+		{
+			switch(status)
+			{
+				case NetStatus.Connected:
+					if(OnConnectionEstablished != null)
+						OnConnectionEstablished.Invoke(this, status);
+					break;
+				case NetStatus.EncryptionEstablished:
+					if(OnConnectionEstablished != null)
+						OnEncryptionEstablished.Invoke(this, status);
+					break;
+			}
+		}
+	}
+}
