2c19a74a6933857e839b50fe2133b6b631bca29f
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-17 21:59:00 -0500

    Simplify Connection Logic

--- src/Booma.Client.Network.Common/Peer/BoomaNetworkClientPeer.cs
+++ src/Booma.Client.Network.Common/Peer/BoomaNetworkClientPeer.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using GladNet.Common;
+using UnityEngine;
 
 namespace Booma.Client.Network.Common
 {
@@ -20,16 +21,6 @@ namespace Booma.Client.Network.Common
 		where TInheritingType : BoomaNetworkClientPeer<TInheritingType>
 	{
 		/// <summary>
-		/// Assets that the generic arg <typeparamref name="TInheritingType"/> is the subtype.
-		/// </summary>
-		private void Awake()
-		{
-			//What a hack
-			if (GetType() != typeof(TInheritingType))
-				throw new InvalidOperationException($"Created invalid closed generic {nameof(BoomaNetworkClientPeer<TInheritingType>)} due to {typeof(TInheritingType)} not being the true subtype of this object.");
-		}
-
-		/// <summary>
 		/// Handler service that will deal with dispatching <see cref="ResponseMessage"/> payloads that are of 
 		/// type <see cref="PacketPayload"/> and will generally have handling logic implemented/abstracted from this peer.
 		/// </summary>
@@ -42,6 +33,30 @@ namespace Booma.Client.Network.Common
 		/// </summary>
 		[Inject]
 		private IEventPayloadHandlerService<TInheritingType> eventHandler { get; }
+
+		/// <summary>
+		/// Assets that the generic arg <typeparamref name="TInheritingType"/> is the subtype.
+		/// </summary>
+		private void Awake()
+		{
+			//What a hack
+			if (GetType() != typeof(TInheritingType))
+				throw new InvalidOperationException($"Created invalid closed generic {nameof(BoomaNetworkClientPeer<TInheritingType>)} due to {typeof(TInheritingType)} not being the true subtype of this object.");
+		}
+
+		[Tooltip("Initilize these fields so that you can connect to a remote application.")]
+		[SerializeField]
+		private PeerEndpointDetails peerConnectDetails;
+
+		/// <summary>
+		/// Simplifies connecting and exposes connecting to <see cref="UnityEvent"/>s for use in the inspector
+		/// Call to connect with the provided <see cref="PeerEndpointDetails"/>.
+		/// </summary>
+		public void Connect()
+		{
+			//Simply call the full parametered connection method.
+			Connect(peerConnectDetails.ComputeServerAddress(), peerConnectDetails.ApplicationName);
+		}
 
 		public sealed override void OnReceiveEvent(PacketPayload payload)
 		{
