a48fc23f3560a84fc3e281f0f9e2079233e61157
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-20 23:09:17 -0500

    Created a shiplist request generator

---
+++ src/Booma.Client.ServerSelection.Authentication/Networking/RequestGenerators/GameServerListRequestGenerator.cs
@@ -0,0 +1,27 @@
+﻿using GladBehaviour.Common;
+using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Booma.Client.ServerSelection.Authentication
+{
+	/// <summary>
+	/// Component is capable of sending gameserver list requests through the
+	/// peer to the currently connected network.
+	/// </summary>
+	public class GameServerListRequestGenerator : GladMonoBehaviour, IRequestSender
+	{
+		//TODO: Put this in a base class.
+		[SerializeField]
+		private IClientPeerNetworkMessageSender messageSender;
+
+		public SendResult SendRequest()
+		{
+			//TODO: Send shiplist request when the payload exists.
+			return SendResult.Invalid;
+		}
+	}
+}
---
+++ src/Booma.Client.ServerSelection.Authentication/Networking/RequestGenerators/IRequestSender.cs
@@ -0,0 +1,21 @@
+﻿using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Client.ServerSelection.Authentication
+{
+	/// <summary>
+	/// Contract for types that provide request sending functionality
+	/// without input.
+	/// </summary>
+	public interface IRequestSender
+	{
+		/// <summary>
+		/// Attempts to send a request.
+		/// </summary>
+		/// <returns>Returns the <see cref="SendResult"/> of the request sending operation.</returns>
+		SendResult SendRequest();
+	}
+}
