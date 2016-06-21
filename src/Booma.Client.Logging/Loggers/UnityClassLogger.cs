8d49947fec6ea8ad123409dfe6060874b9d807bf
Andrew Blakely <andrew.blakely@ymail.com>
2016-06-21 01:48:34 -0500

    Fixed Incorrect Logger design

--- src/Booma.Client.Logging/Loggers/UnityClassLogger.cs
+++ src/Booma.Client.Logging/Loggers/UnityClassLogger.cs
@@ -13,57 +13,48 @@ namespace Booma.Client.Logging
 	/// logging API. Conforms to the <see cref="ILog"/> <see cref="Common.Logging"/> interface
 	/// specification and implementation.
 	/// </summary>
-	public class UnityClassLogger : IClassLogger
+	public class UnityLoggingService : AbstractSimpleLogger
 	{
-		//used internally to match the ILog interface.
-		private class UnityLogger : AbstractSimpleLogger
+		public UnityLoggingService()
+			: base("UnityLogger", LogLevel.All, false, false, false, "")
 		{
-			public UnityLogger()
-				: base("UnityLogger", LogLevel.All, false, false, false, "")
-			{
 
-			}
-
-			private UnityLogger(string logName, LogLevel logLevel, bool showlevel, bool showDateTime, bool showLogName, string dateTimeFormat) 
-				: base(logName, logLevel, showlevel, showDateTime, showLogName, dateTimeFormat)
-			{
-				//None of these parameters matter.
-			}
-
-			protected override void WriteInternal(LogLevel level, object message, Exception exception)
-			{
-				switch (level)
-				{
-					case LogLevel.Trace:
-						UnityEngine.Debug.Log(message);
-						break;
-					case LogLevel.Debug:
-						UnityEngine.Debug.Log(message);
-						break;
-					case LogLevel.Info:
-						UnityEngine.Debug.Log(message);
-						break;
-					case LogLevel.Warn:
-						UnityEngine.Debug.LogWarning(message);
-						break;
-					case LogLevel.Error:
-						UnityEngine.Debug.LogError(message);
-						break;
-					case LogLevel.Fatal:
-						UnityEngine.Debug.LogError(message);
-						break;
-					case LogLevel.Off:
-						break;
-					default:
-						UnityEngine.Debug.Log(message);
-						break;
-				}
-			}
 		}
 
-		/// <summary>
-		/// Unity3D logging service.
-		/// </summary>
-		public ILog Logger { get; } = new UnityLogger();
+		private UnityLoggingService(string logName, LogLevel logLevel, bool showlevel, bool showDateTime, bool showLogName, string dateTimeFormat)
+				: base(logName, logLevel, showlevel, showDateTime, showLogName, dateTimeFormat)
+		{
+			//None of these parameters matter.
+		}
+
+		protected override void WriteInternal(LogLevel level, object message, Exception exception)
+		{
+			switch (level)
+			{
+				case LogLevel.Trace:
+					UnityEngine.Debug.Log(message);
+					break;
+				case LogLevel.Debug:
+					UnityEngine.Debug.Log(message);
+					break;
+				case LogLevel.Info:
+					UnityEngine.Debug.Log(message);
+					break;
+				case LogLevel.Warn:
+					UnityEngine.Debug.LogWarning(message);
+					break;
+				case LogLevel.Error:
+					UnityEngine.Debug.LogError(message);
+					break;
+				case LogLevel.Fatal:
+					UnityEngine.Debug.LogError(message);
+					break;
+				case LogLevel.Off:
+					break;
+				default:
+					UnityEngine.Debug.Log(message);
+					break;
+			}
+		}
 	}
 }
