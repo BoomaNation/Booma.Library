1e55a8275275a28461d2d8358146264fe99a3aec
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-06 01:27:40 -0500

    Updated to GladNet2 2.x

--- src/Booma.Client.Network.Common/IoC/NetworkMessageHandlerServiceRegistration.cs
+++ src/Booma.Client.Network.Common/IoC/NetworkMessageHandlerServiceRegistration.cs
@@ -1,5 +1,8 @@
 ﻿using GladLive.Common;
 using GladNet.Common;
+using GladNet.Engine.Common;
+using GladNet.Message;
+using GladNet.Message.Handlers;
 using SceneJect.Common;
 using System;
 using System.Collections.Generic;
@@ -15,18 +18,19 @@ namespace Booma.Client.Network.Common
 	/// Handlers are indicated by generic type arg 22 <typeparamref name="TNetworkHandlerType"/>
 	/// </summary>
 	/// <typeparam name="TPeerType"></typeparam>
-	public abstract class NetworkMessageHandlerServiceRegistration<TPeerType, TNetworkHandlerType, THandlerServiceTypeConcrete, THandlerServiceTypeServiceInterface> : NonBehaviourDependency
-		where TPeerType : INetPeer 
-		where TNetworkHandlerType : IPayloadHandler<TPeerType>
-		where THandlerServiceTypeConcrete : class, TNetworkHandlerType, IPayloadHandler<TPeerType>, THandlerServiceTypeServiceInterface //there is no interface for handler services so we must contrain against payloadhandler
-		where THandlerServiceTypeServiceInterface : IPayloadHandler<TPeerType>
+	public abstract class NetworkMessageHandlerServiceRegistration<TPeerType, TNetworkMessageType, TNetworkHandlerType, THandlerServiceTypeConcrete, THandlerServiceTypeServiceInterface> : NonBehaviourDependency
+		where TPeerType : INetPeer
+		where TNetworkMessageType : INetworkMessage
+		where TNetworkHandlerType : IMessageHandler<TPeerType, TNetworkMessageType>
+		where THandlerServiceTypeConcrete : class, TNetworkHandlerType, THandlerServiceTypeServiceInterface //there is no interface for handler services
+		where THandlerServiceTypeServiceInterface : IMessageHandler<TPeerType, TNetworkMessageType>
 	{
 		public override void Register(IServiceRegister register)
 		{
 			//Chain handling semantics are preferable on the client.
 			//No reason multiple handlers should be consuming the same packets, complications things
 			//Put it infront of the handling process if you want that functionality
-			ChainPayloadHandler<TPeerType> chainHandler = new ChainPayloadHandler<TPeerType>(FindHandlersInScene());
+			ChainMessageHandlerStrategy<TPeerType, TNetworkMessageType> chainHandler = new ChainMessageHandlerStrategy<TPeerType, TNetworkMessageType>(FindHandlersInScene());
 
 			//generics are semi-limited in .Net when trying to construct an instance of the type so we put the requirement for inheritors to create the instance.
 			register.Register<THandlerServiceTypeConcrete>(CreateConcreteService(chainHandler), getFlags(), typeof(THandlerServiceTypeServiceInterface));
@@ -38,30 +42,30 @@ namespace Booma.Client.Network.Common
 		/// </summary>
 		/// <param name="strat"></param>
 		/// <returns></returns>
-		protected abstract THandlerServiceTypeConcrete CreateConcreteService(IPayloadHandlerStrategy<TPeerType> strat);
+		protected abstract THandlerServiceTypeConcrete CreateConcreteService(IMessageHandlerStrategy<TPeerType, TNetworkMessageType> strat);
 
 		/// <summary>
 		/// Locates references to all <see cref="MonoBehaviour"/>s in the current scene that implement
 		/// <see cref="IPayloadHandler{TPeerType}"/>
 		/// </summary>
 		/// <returns></returns>
-		private IEnumerable<IPayloadHandler<TPeerType>> FindHandlersInScene()
+		private IEnumerable<IMessageHandler<TPeerType, TNetworkMessageType>> FindHandlersInScene()
 		{
 			//Used for debugging for count of handlers in the scene
 			//The type co/contra variance in Unity is iffy
 #if DEBUG || DEBUG_BUILD || DEBUGBUILD
-			IEnumerable<IPayloadHandler<TPeerType>> monoBehaviours =
+			IEnumerable<IMessageHandler<TPeerType, TNetworkMessageType>> monoBehaviours =
 #else
 				return
 #endif
 			Resources.FindObjectsOfTypeAll<MonoBehaviour>()
 				.Where(mb => mb.GetType().GetInterfaces().Where(t => typeof(TNetworkHandlerType).IsAssignableFrom(t)).Count() != 0 ) //this checks to see if the handler we found is assignable to a potentially less derived interface type. (it's co? or maybe contra? variant)
-				.Cast<IPayloadHandler<TPeerType>>();
+				.Cast<IMessageHandler<TPeerType, TNetworkMessageType>>();
 
 #if DEBUG || DEBUG_BUILD || DEBUGBUILD
 			Debug.Log($"{monoBehaviours.Count()}");
 
-			return monoBehaviours.Cast<IPayloadHandler<TPeerType>>();
+			return monoBehaviours.Cast<IMessageHandler<TPeerType, TNetworkMessageType>>();
 #endif
 		}
 	}
--- src/Booma.Client.Network.Common/Network/EventPayloadHandlerComponent.cs
+++ src/Booma.Client.Network.Common/Network/EventPayloadHandlerComponent.cs
@@ -1,6 +1,10 @@
 ﻿using Common.Logging;
 using GladLive.Common;
 using GladNet.Common;
+using GladNet.Engine.Common;
+using GladNet.Message;
+using GladNet.Message.Handlers;
+using GladNet.Payload;
 using SceneJect.Common;
 using System;
 using System.Collections.Generic;
@@ -11,13 +15,13 @@ using UnityEngine;
 namespace Booma.Client.Network.Common
 {
 	/// <summary>
-	/// Scene-based <see cref="IEventPayloadHandler{TPeerType, TPayloadType}"/>
+	/// Scene-based <see cref="IEventMessageHandler{TPeerType}"/>
 	/// </summary>
 	/// <typeparam name="TPeerType">Type of peer this event was recieved on.</typeparam>
 	/// <typeparam name="TPayloadType">Type of payload that this handler can handle.</typeparam>
-	public abstract class EventPayloadHandlerComponent<TPeerType, TPayloadType> : MonoBehaviour, IEventPayloadHandler<TPeerType, TPayloadType>, IClassLogger
-		where TPayloadType : PacketPayload
+	public abstract class EventPayloadHandlerComponent<TPeerType, TPayloadType> : MonoBehaviour, IEventMessageHandler<TPeerType>, IClassLogger
 		where TPeerType : INetPeer
+		where TPayloadType : PacketPayload
 	{
 		/// <summary>
 		/// Logging service for handlers.
@@ -26,43 +30,26 @@ namespace Booma.Client.Network.Common
 		public ILog Logger { get; private set; }
 
 		/// <summary>
-		/// Attempts to handle the loosely-typed payload.
-		/// Will return false if <see cref="PacketPayload"/> isn't a <typeparamref name="TPayloadType"/>.
-		/// </summary>
-		/// <param name="payload">Packet payload to be handled.</param>
-		/// <param name="parameters">Message parameters (null with Photon)</param>
-		/// <param name="peer">The peer that recieved this <see cref="PacketPayload"/>.</param>
-		/// <returns>True if the payload was handled. False if it was not.</returns>
-		public bool TryProcessPayload(PacketPayload payload, IMessageParameters parameters, TPeerType peer)
-		{
-			//Just try to as cast it and chek null in the other
-			return TryProcessPayload(payload as TPayloadType, parameters, peer);
-		}
-
-		/// <summary>
-		/// Attempts to handle the <typeparamref name="TPayloadType"/>.
+		/// Handles the <see cref="IEventMessage"/> and specified <typeparamref name="TPayloadType"/>.
 		/// </summary>
 		/// <param name="payload">Packet payload to be handled.</param>
 		/// <param name="parameters">Message parameters (null with Photon)</param>
 		/// <param name="peer">The peer that recieved this <see cref="TPayloadType"/> payload.</param>
-		/// <returns>True if the payload was handled. False if it was not.</returns>
-		public bool TryProcessPayload(TPayloadType payload, IMessageParameters parameters, TPeerType peer)
+		protected abstract void OnIncomingHandlableMessage(IEventMessage message, TPayloadType payload, IMessageParameters parameters, TPeerType peer);
+
+		public bool TryProcessMessage(IEventMessage message, IMessageParameters parameters, TPeerType peer)
 		{
+			TPayloadType payload = message.Payload.Data as TPayloadType;
+
+			//if it's the not the payload type this handler handles then we
+			//indicate non-consumption
 			if (payload == null)
 				return false;
 			else
-				HandleNonNullStronglyTypedPayload(payload, parameters, peer);
+				OnIncomingHandlableMessage(message, payload, parameters, peer);
 
-			//at this point it SHOULD have been handled since we can handle this type
+			//If an exception wasn't thrown we'll be indicating that the payload has been consumed.
 			return true;
 		}
-
-		/// <summary>
-		/// Handlesthe <typeparamref name="TPayloadType"/>.
-		/// </summary>
-		/// <param name="payload">Packet payload to be handled.</param>
-		/// <param name="parameters">Message parameters (null with Photon)</param>
-		/// <param name="peer">The peer that recieved this <see cref="TPayloadType"/> payload.</param>
-		protected abstract void HandleNonNullStronglyTypedPayload(TPayloadType payload, IMessageParameters parameters, TPeerType peer);
 	}
 }
