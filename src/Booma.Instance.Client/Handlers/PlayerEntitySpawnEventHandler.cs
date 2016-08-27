4a318936b47ad4ea10b2729a44a01536c5313a3a
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-27 15:11:47 -0500

    Create client handler and peer

---
+++ src/Booma.Instance.Client/Handlers/PlayerEntitySpawnEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Booma.Client.Network.Common;
+using Booma.Payloads.Instance;
+using GladNet.Message.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet.Message;
+using Booma.Instance.Common;
+using SceneJect.Common;
+using Common.Logging;
+
+namespace Booma.Instance.Client
+{
+	[Injectee]
+	public class PlayerEntitySpawnEventHandler : EventPayloadHandlerComponent<InstanceClientPeer, PlayerSpawnEventPayload>
+	{
+		//[Inject]
+		//private readonly IEntityFactory entityFactory;
+
+		[Inject]
+		private readonly ILog logger;
+
+		protected override void OnIncomingHandlableMessage(IEventMessage message, PlayerSpawnEventPayload payload, IMessageParameters parameters, InstanceClientPeer peer)
+		{
+			//TODO: Implement spawning
+			logger.Info($"Recieved spawn event for ID: {payload.EntityId}.");
+		}
+	}
+}
---
+++ src/Booma.Instance.Client/Peer/InstanceClientPeer.cs
@@ -0,0 +1,26 @@
+﻿using Booma.Client.Network.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet.Common;
+using GladNet.Serializer;
+using GladNet.Message;
+using Booma.Payloads.Instance;
+
+namespace Booma.Instance.Client
+{
+	public class InstanceClientPeer : BoomaNetworkClientPeer<InstanceClientPeer>
+	{
+		public override void OnStatusChanged(NetStatus status)
+		{
+			//TODO: Implement status handling
+		}
+
+		public override void RegisterPayloadTypes(ISerializerRegistry registry)
+		{
+			registry.Register(typeof(NetworkMessage));
+			registry.Register(typeof(EntitySpawnEventPayload));
+		}
+	}
+}
