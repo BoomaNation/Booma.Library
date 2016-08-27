8d01c6fd01e3f05cd3d91ed0d46a446164a579c4
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-26 22:50:31 -0500

    Add EntityCollections

---
+++ src/Booma.Instance.Common/Entity/IEntityCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Instance.Common
+{
+	public interface IEntityCollection<TEntityType> : IDictionary<int, TEntityType>
+	{
+		/// <summary>
+		/// Indicates the type of entity this collection manages.
+		/// </summary>
+		EntityType CollectionType { get; }
+	}
+}
---
+++ src/Booma.Instance.Common/Entity/PlayerEntityCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Booma.Instance.Common
+{
+	public class PlayerEntityCollection : Dictionary<int, GameObject>, IEntityCollection<GameObject>
+	{
+		/// <summary>
+		/// Indicates the type of entity this collection manages.
+		/// </summary>
+		public EntityType CollectionType { get; } = EntityType.Player;
+	}
+}
