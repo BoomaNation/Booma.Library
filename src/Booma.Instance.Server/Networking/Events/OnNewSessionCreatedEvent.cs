d0403ca3d33380e0f6f87efd53d5ce4ca56de73d
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-26 22:50:02 -0500

    Add Session Events

---
+++ src/Booma.Instance.Server/Networking/Events/OnNewSessionCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using GladNet.Engine.Common;
+using GladNet.Engine.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.Events;
+
+namespace Booma.Instance.Server
+{
+	/// <summary>
+	/// Event for new session creation.
+	/// </summary>
+	public class OnNewSessionCreatedEvent : UnityEvent<ClientPeerSession> { }
+}
