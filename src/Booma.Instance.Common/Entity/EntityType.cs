baa794e5cf4a1b4c8883517b0a5ed107e43f9cf4
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-26 22:50:16 -0500

    Add EntityType

---
+++ src/Booma.Instance.Common/Entity/EntityType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Instance.Common
+{
+	public enum EntityType : int
+	{
+		Player = 0,
+	}
+}
