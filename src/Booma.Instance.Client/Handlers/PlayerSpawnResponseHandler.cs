3fd7958e7e41c822e942defc42d2eb529d75d8e0
HelloKitty <andrew.blakely@ymail.com>
2016-10-11 22:25:50 -0500

    Add PlayerSpawnResposneHandler

---
+++ src/Booma.Instance.Client/Handlers/PlayerSpawnResponseHandler.cs
@@ -0,0 +1,27 @@
+﻿using Booma.Client.Network.Common;
+using Booma.Payloads.Instance;
+using GladNet.Message.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet.Message;
+using Booma.Instance.Common;
+using SceneJect.Common;
+using Common.Logging;
+
+namespace Booma.Instance.Client
+{
+	[Injectee]
+	public class PlayerSpawnResponseHandler : ResponsePayloadHandlerComponent<InstanceClientPeer, PlayerSpawnResponsePayload>
+	{
+		[Inject]
+		private readonly ILog logger;
+
+		protected override void OnIncomingHandlableMessage(IResponseMessage message, PlayerSpawnResponsePayload payload, IMessageParameters parameters, InstanceClientPeer peer)
+		{
+			//TODO: Implement spawning
+			logger.Info($"Recieved player spawn response.");
+		}
+	}
+}
