8908920416351da2928a171d8c3a1b12ace85e0f
HelloKitty <andrew.blakely@ymail.com>
2016-12-17 16:16:28 -0600

    Added Current Position to MoveRequestPayload

--- src/Booma.Payloads.Instance/Payloads/Requests/PlayerMoveRequestPayload.cs
+++ src/Booma.Payloads.Instance/Payloads/Requests/PlayerMoveRequestPayload.cs
@@ -22,15 +22,22 @@ namespace Booma.Payloads.Instance
 		public Vector3Surrogate Direction { get; private set; }
 
 		/// <summary>
+		/// Represents the position that the requestee thinks it is in. It may have mispredicted this position so it could be wrong.
+		/// </summary>
+		[GladNetMember(GladNetDataIndex.Index2)]
+		public Vector3Surrogate CurrentPosition { get; private set; }
+
+		/// <summary>
 		/// Creates a new <see cref="BoomaPayloadMessageType.PlayerMoveRequestPayload"/> payload.
 		/// </summary>
-		public PlayerMoveRequestPayload(Vector3Surrogate direction)
+		public PlayerMoveRequestPayload(Vector3Surrogate direction, Vector3Surrogate currentPosition)
 		{
 			//TODO: Check refs
 
 			//Do not normalize on client. No point. Server will need to renormalize anyway
 			//Malicious clients could send non-normalized directions
 			Direction = direction;
+			CurrentPosition = currentPosition;
 		}
 
 		/// <summary>
