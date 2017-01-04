cbdb8b61f1129297757443abd7f1642f6546f0e9
HelloKitty <andrew.blakely@ymail.com>
2017-01-04 01:24:23 -0600

    Add Serializer setup for NetworkEntityGuid from interface

--- src/Booma.Entity.Identity/Guid/IEntityIdentifiable.cs
+++ src/Booma.Entity.Identity/Guid/IEntityIdentifiable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GladNet.Serializer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Booma.Entity.Identity
 	/// <summary>
 	/// Contract for types that can indentify a Entity.
 	/// </summary>
+	[GladNetSerializationContract]
+	[GladNetSerializationInclude(GladNetIncludeIndex.Index1, typeof(NetworkEntityGuid))]
 	public interface IEntityIdentifiable
 	{
 		/// <summary>
