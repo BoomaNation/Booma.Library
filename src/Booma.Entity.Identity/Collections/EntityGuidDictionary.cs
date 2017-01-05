c3bbc72835b4de7df8ef5ef46c7d844da62ebf22
HelloKitty <andrew.blakely@ymail.com>
2017-01-04 18:23:12 -0600

    Added NetworkEntityGuid Dictionary

---
+++ src/Booma.Entity.Identity/Collections/EntityGuidDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Entity.Identity
+{
+	/// <summary>
+	/// Generic dictionary with <see cref="NetworkEntityGuid"/> key types.
+	/// </summary>
+	/// <typeparam name="TKey">Key type.</typeparam>
+	/// <typeparam name="TValue">Value type.</typeparam>
+	public class EntityGuidDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+		where TKey : NetworkEntityGuid
+	{
+		public EntityGuidDictionary()
+			: base(NetworkGuidEqualityComparer<TKey>.Instance)
+		{
+
+		}
+
+		public EntityGuidDictionary(int capacity)
+			: base(capacity, NetworkGuidEqualityComparer<TKey>.Instance)
+		{
+
+		}
+
+		public EntityGuidDictionary(IDictionary<TKey, TValue> dictionary)
+			: base(dictionary, NetworkGuidEqualityComparer<TKey>.Instance)
+		{
+
+		}
+	}
+}
---
+++ src/Booma.Entity.Identity/Guid/NetworkGuidEqualityComparer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Entity.Identity
+{
+	/// <summary>
+	/// Implements simple value-type style comparision semantics for the <see cref="NetworkEntityGuid"/>
+	/// type.
+	/// </summary>
+	public class NetworkGuidEqualityComparer<TType> : EqualityComparer<TType>
+		where TType : NetworkEntityGuid
+	{
+		/// <summary>
+		/// Singleton instance of the <see cref="NetworkGuidEqualityComparer"/>.
+		/// </summary>
+		public static NetworkGuidEqualityComparer<TType> Instance { get; } = new NetworkGuidEqualityComparer<TType>();
+
+		//Forces use of singleton
+		protected NetworkGuidEqualityComparer()
+		{
+
+		}
+
+		public override bool Equals(TType x, TType y)
+		{
+			if (x == null)
+			{
+				if (y == null)
+					return true;
+			}
+
+			return y.Equals(x);
+		}
+
+		public override int GetHashCode(TType obj)
+		{
+			//Just return the ulong hash code.
+			return obj.RawGuidValue.GetHashCode();
+		}
+	}
+}
