dc24b9f73b61b9a938d6d1468768f5980a827c27
HelloKitty <andrew.blakely@ymail.com>
2017-01-07 02:47:18 -0600

    Added IEntityGuidContainer

---
+++ src/Booma.Entity.Identity/Guid/IEntityGuidContainer.cs
@@ -0,0 +1,19 @@
+﻿using GladNet.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Entity.Identity
+{
+	/// <summary>
+	/// Contract for types that contain a <see cref="NetworkEntityGuid"/>
+	/// </summary>
+	public interface IEntityGuidContainer
+	{
+		/// <summary>
+		/// The Network GUID contained inside the container.
+		/// </summary>
+		NetworkEntityGuid NetworkGuid { get; }
+	}
+}
