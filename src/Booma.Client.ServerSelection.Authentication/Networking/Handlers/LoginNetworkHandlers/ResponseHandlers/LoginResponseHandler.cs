1a2c5bc96aa3cfeacb4cb469d4f57aaaf29435e2
HelloKitty <andrew.blakely@ymail.com>
2017-05-05 17:25:34 -0500

    Vs2017 update; updated nugets; cleanedup

--- src/Booma.Client.ServerSelection.Authentication/Networking/Handlers/LoginNetworkHandlers/ResponseHandlers/LoginResponseHandler.cs
+++ src/Booma.Client.ServerSelection.Authentication/Networking/Handlers/LoginNetworkHandlers/ResponseHandlers/LoginResponseHandler.cs
@@ -7,57 +7,41 @@ using GladNet.Common;
 using UnityEngine;
 using Booma.Client.Network.Common;
 using SceneJect.Common;
-using GladLive.Common.Payloads;
 using UnityEngine.Events;
 using GladNet.Message;
+using GladNet.Payload.Authentication;
 
 namespace Booma.Client.ServerSelection.Authentication
 {
 	/// <summary>
 	/// Response handler that handless <see cref="LoginResponse"/> payloads.
 	/// </summary>
-	public class LoginResponseHandler : ResponsePayloadHandlerComponent<LoginNetworkClient, LoginResponse>
+	public class LoginResponseHandler : ResponsePayloadHandlerComponent<AuthenticationWebClient, AuthenticationResponse>
 	{
 		/// <summary>
 		/// Serializable hack for getting UnityEvents in the Unity inspector
 		/// </summary>
 		[Serializable]
-		public class Event : UnityEvent<LoginResponseCode> { }
+		public class ErrorEvent : UnityEvent<ResponseErrorCode, string> { }
 
 		/// <summary>
 		/// Invoked on a failed <see cref="LoginResponse"/> handled.
 		/// </summary>
 		[SerializeField]
-		private Event OnFailure;
+		private ErrorEvent OnFailure;
 
 		/// <summary>
 		/// Invoked on a sucessful <see cref="LoginResponse"/> handled.
 		/// </summary>
 		[SerializeField]
-		private Event OnSuccess;
+		private UnityEvent OnSuccess;
 
-		protected override void OnIncomingHandlableMessage(IResponseMessage message, LoginResponse payload, IMessageParameters parameters, LoginNetworkClient peer)
+		protected override void OnIncomingHandlableMessage(IResponseMessage message, AuthenticationResponse payload, IMessageParameters parameters, AuthenticationWebClient peer)
 		{
-			//Log out the response first
-			Logger.Debug($"Recieved response code: {payload.Code}");
-
-
-			switch (payload.Code)
-			{
-				case LoginResponseCode.Success:
-					if (OnSuccess != null)
-						OnSuccess.Invoke(payload.Code);
-					break;
-				case LoginResponseCode.Banned:
-				case LoginResponseCode.Locked:
-				case LoginResponseCode.Failed:
-				case LoginResponseCode.AuthServerUnavailable:
-					if (OnFailure != null)
-						OnFailure.Invoke(payload.Code);
-					break;
-				default:
-					throw new InvalidOperationException($"Recieved invalid or unknown response code {payload.Code}.");
-			}
+			if (payload.AuthenticationSuccessful)
+				OnSuccess?.Invoke();
+			else
+				OnFailure?.Invoke(payload.OptionalError ?? ResponseErrorCode.Error, payload.OptionalErrorMessage);
 		}
 	}
 }
--- src/Booma.Client.ServerSelection.Authentication/Networking/RequestGenerators/GameServerListRequestGenerator.cs
+++ src/Booma.Client.ServerSelection.Authentication/Networking/RequestGenerators/GameServerListRequestGenerator.cs
@@ -1,10 +1,13 @@
-﻿using GladBehaviour.Common;
+﻿using Booma.Payloads.ServerSelection;
+using Common.Logging;
+using GladBehaviour.Common;
 using GladNet.Common;
 using GladNet.Engine.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SceneJect.Common;
 using UnityEngine;
 
 namespace Booma.Client.ServerSelection.Authentication
@@ -17,7 +20,19 @@ namespace Booma.Client.ServerSelection.Authentication
 	{
 		//TODO: Put this in a base class.
 		[SerializeField]
-		private IClientPeerPayloadSender messageSender;
+		private INetPeer messageSender;
+
+		[Inject]
+		private ILog logger;
+
+		public void Start()
+		{
+			if (messageSender == null)
+				throw new InvalidOperationException($"Field {nameof(messageSender)} was null.");
+
+			if (logger == null)
+				throw new InvalidOperationException($"Field {nameof(logger)} was null.");
+		}
 
 		public void SendRequest()
 		{
@@ -26,8 +41,10 @@ namespace Booma.Client.ServerSelection.Authentication
 
 		public SendResult SendRequestWithResult()
 		{
-			//TODO: Send shiplist request when the payload exists.
-			return SendResult.Invalid;
+			if(logger.IsDebugEnabled)
+				logger.Debug("Sending list request.");
+
+			return messageSender.TrySendMessage(OperationType.Request, new GameServerListRequestPayload(), DeliveryMethod.ReliableOrdered, true);
 		}
 	}
 }
