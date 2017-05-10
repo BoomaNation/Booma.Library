08bca579a55d98195f8673f87e455007f250059e
HelloKitty <andrew.blakely@ymail.com>
2017-05-10 16:46:14 -0500

    Added SectionId enumeration

---
+++ src/Booma.Entity.Character/Enums/SectionId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Booma.Entity.Character
+{
+	/// <summary>
+	/// Enumeration of all section IDs.
+	/// See: http://phantasystar.wikia.com/wiki/Section_ID
+	/// </summary>
+	public enum SectionId : int
+	{
+		Viridia,
+		Greenill,
+		Skyly,
+		Bluefull,
+		Purplenum,
+		Pinkal,
+		Redria,
+		Oran,
+		Yellowboze,
+		Whitill
+	}
+}
