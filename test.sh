df6a3aa77798c2bd5ea697a7a2c15f542b492d8c
HelloKitty <andrew.blakely@ymail.com>
2017-05-16 20:06:06 -0500

    Try to fix Travis exit code collection

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 result=0
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Combat.Formula.Server.Tests/bin/Release/Booma.Combat.Formula.Server.Tests.dll
-result= result | $?
+result= result || $?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Entity.Tests/bin/Release/Booma.Entity.Tests.dll
-result= result | $?
+result= result || $?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Payloads.Tests/bin/Release/Booma.Payloads.Tests.dll
-result= result | $?
+result= result || $?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Stats.Common.Tests/bin/Release/Booma.Stats.Common.Tests.dll
-result= result | $?
+result= result || $?
 
 exit result
