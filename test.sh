18369536cce1440519adfdb49f64749d443c4de2
HelloKitty <andrew.blakely@ymail.com>
2017-05-16 19:45:54 -0500

    Combine exit code from all tests

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Combat.Formula.Server.Tests/bin/Release/Booma.Combat.Formula.Server.Tests.dll
-mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Entity.Tests/bin/Release/Booma.Entity.Tests.dll
-mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Payloads.Tests/bin/Release/Booma.Payloads.Tests.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Combat.Formula.Server.Tests/bin/Release/Booma.Combat.Formula.Server.Tests.dll ||
+mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Entity.Tests/bin/Release/Booma.Entity.Tests.dll ||
+mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Payloads.Tests/bin/Release/Booma.Payloads.Tests.dll ||
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Stats.Common.Tests/bin/Release/Booma.Stats.Common.Tests.dll
+
+exit $?
