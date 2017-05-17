bbe5ae72a545cc868b5490cbc9869122031796a9
HelloKitty <andrew.blakely@ymail.com>
2017-05-16 21:18:29 -0500

    Try to fix nunit on travis

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
-result=0
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Combat.Formula.Server.Tests/bin/Release/Booma.Combat.Formula.Server.Tests.dll
-result= result || $?
+result1=$?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Entity.Tests/bin/Release/Booma.Entity.Tests.dll
-result= result || $?
+result2=$?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Payloads.Tests/bin/Release/Booma.Payloads.Tests.dll
-result= result || $?
+result3=$?
 
 mono ./testrunner/NUnit.ConsoleRunner.3.6.1/tools/nunit3-console.exe ./test/Booma.Stats.Common.Tests/bin/Release/Booma.Stats.Common.Tests.dll
-result= result || $?
+result4=$?
 
-exit result
+exit result1 || result2 || result3 || result4
