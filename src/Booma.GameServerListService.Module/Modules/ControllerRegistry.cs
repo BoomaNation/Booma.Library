dc34d4be9cd5a2f915b1b83e2b753a61296bcc99
HelloKitty <andrew.blakely@ymail.com>
2017-05-17 18:22:53 -0500

    Added GameServerList module library

---
+++ src/Booma.GameServerListService.Module/Modules/ControllerRegistry.cs
@@ -0,0 +1,23 @@
+﻿using Booma.GameServerList.Lib;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Booma.GameServerList.Module
+{
+	public class ControllerRegistry : IApplicationFeatureProvider<ControllerFeature>
+	{
+		public void PopulateFeature([NotNull] IEnumerable<ApplicationPart> parts, [NotNull] ControllerFeature feature)
+		{
+			if (parts == null) throw new ArgumentNullException(nameof(parts));
+			if (feature == null) throw new ArgumentNullException(nameof(feature));
+
+			feature.Controllers.Add(typeof(GameListRequestController).GetTypeInfo());
+		}
+	}
+}
