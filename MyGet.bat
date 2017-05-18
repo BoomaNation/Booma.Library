acd9c0b88619c234d8e16f74bb5af02baded9421
HelloKitty <andrew.blakely@ymail.com>
2017-05-18 17:01:30 -0500

    Remove pause that is stalling myget

--- MyGet.bat
+++ MyGet.bat
@@ -4,4 +4,3 @@ nuget.exe spec src/Build.All/Build.All.csproj
 nuget.exe spec src/Booma.Entity.Identity/Booma.Entity.Identity.csproj
 nuget.exe pack src/Booma.Entity.Identity/Booma.Entity.Identity.csproj -IncludeReferencedProjects
 nuget.exe pack src/Build.All/Build.All.csproj -IncludeReferencedProjects
-PAUSE
