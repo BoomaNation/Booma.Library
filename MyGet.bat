bfcc1d3f6abe3f96039b7097025a4f043b2983c8
HelloKitty <andrew.blakely@ymail.com>
2017-05-18 18:01:12 -0500

    Cleanup myget to only restore/build

--- MyGet.bat
+++ MyGet.bat
@@ -1,6 +1,2 @@
 nuget.exe restore Booma.Library.sln -NoCache -NonInteractive -ConfigFile Nuget.config
 msbuild Booma.Library.sln /p:Configuration=Release
-nuget.exe spec src/Build.All/Build.All.csproj
-nuget.exe spec src/Booma.Entity.Identity/Booma.Entity.Identity.csproj
-nuget.exe pack src/Booma.Entity.Identity/Booma.Entity.Identity.csproj -IncludeReferencedProjects
-nuget.exe pack src/Build.All/Build.All.csproj -IncludeReferencedProjects
