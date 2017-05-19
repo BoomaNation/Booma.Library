bc8c7cf921b9d5b211b45f76317b14bcfda10053
HelloKitty <andrew.blakely@ymail.com>
2017-05-18 19:43:10 -0500

    Add a wait for nuget in the myget powershell

--- post-MyGet.ps1
+++ post-MyGet.ps1
@@ -8,6 +8,6 @@ foreach($f in Get-ChildItem ./src/)
         ##Add the project path + the csproj name and add the include referenced projects argument which will
         ##force nuget dependencies
         $projectArgs = "pack " + (Join-Path (Join-Path src/ $f.Name) $ff.Name) + " -IncludeReferencedProjects"
-        Start-Process Nuget.exe $projectArgs
+        Start-Process Nuget.exe $projectArgs -Wait
     }
 }
