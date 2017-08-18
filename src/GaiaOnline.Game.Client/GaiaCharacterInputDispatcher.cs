e2cf514f63847e69abf599a30273a468b8d5adcc
HelloKitty <andrew.blakely@ymail.com>
2017-08-18 18:37:38 -0500

    Committing Prototype Project Classes
    This is a crappy commit but I'm going to commit now and commit more as I
    clean up the remamining prototype classes.

---
+++ src/GaiaOnline.Game.Client/GaiaCharacterInputDispatcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GaiaCharacterInputDispatcher : MonoBehaviour
+{
+	[Serializable]
+	private class OnDirectionChangedEvent : UnityEvent<Vector2> { }
+
+	/// <summary>
+	/// Event dispatched when movement starts.
+	/// </summary>
+	[SerializeField]
+	private UnityEvent OnStartMoving;
+
+	/// <summary>
+	/// Event dispatched when movement stops.
+	/// </summary>
+	[SerializeField]
+	private UnityEvent OnStopMoving;
+
+	/// <summary>
+	/// Event dispatched when the direction changes.
+	/// </summary>
+	[SerializeField]
+	private OnDirectionChangedEvent OnDirectionChanged;
+
+	//locally cached movement state
+	private bool isMoving;
+
+	//locally cached direction
+	private Vector2 direction;
+
+	//Must do in update, not fixed update
+	void Update ()
+	{
+		//TODO: Extract this into an input controller. We should only dispatch here
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		//If our movement state isn't matching the input then negate
+		if (isMoving != input.magnitude > 0.0f)
+		{
+			if(isMoving)
+				OnStopMoving?.Invoke();
+			else
+				OnStartMoving?.Invoke();
+
+			isMoving = !isMoving;
+		}
+
+		//Now we need to set the direction if it's dirrection
+		if (direction != input)
+		{
+			OnDirectionChanged?.Invoke(input);
+
+			direction = input;
+		}
+	}
+}
---
+++ src/GaiaOnline.Game.Common/Extensions/Vector3Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GaiaOnline
+{
+	/// <summary>
+	/// Extension methods for the <see cref="Vector3"/> Type.
+	/// </summary>
+	public static class VectorExtensions
+	{
+		/// <summary>
+		/// Converts a <see cref="Vector3"/> into a <see cref="Vector2"/> without the z component.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>The <see cref="Vector2"/> representation of the provided <see cref="Vector3"/> with x and y mapping.</returns>
+		public static Vector2 ToVector2(this Vector3 vector)
+		{
+			return new Vector2(vector.x, vector.y);
+		}
+
+		/// <summary>
+		/// Converts a <see cref="Vector2"/> into a <see cref="Vector3"/> with the z component as 0.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>The <see cref="Vector3"/> representation of the provided <see cref="Vector2"/> with x and y mapping and z as 0.</returns>
+		public static Vector3 ToVector3(this Vector2 vector)
+		{
+			return new Vector3(vector.x, vector.y);
+		}
+
+		/// <summary>
+		/// Converts a <see cref="Vector2"/> into a <see cref="Vector3"/> with the z component as the y component
+		/// and the x component as the x component. y will be 0.
+		/// </summary>
+		/// <param name="vector">The vector to convert.</param>
+		/// <returns>The <see cref="Vector3"/> representation of the provided <see cref="Vector2"/> with x and y mapping to x and z respectively. Y being 0.</returns>
+		public static Vector3 ToVector3XZ(this Vector2 vector)
+		{
+			return new Vector3(vector.x, 0.0f, vector.y);
+		}
+	}
+}
