29a94948c3e1d9c38656c91e1af2d9694d8be299
HelloKitty <andrew.blakely@ymail.com>
2017-08-18 22:21:12 -0500

    Added some documentation to the UserAvatarQueryResponse

--- src/GaiaOnline.Game.Network.Client/GaiaServices/Models/UserAvatarQueryResponse.cs
+++ src/GaiaOnline.Game.Network.Client/GaiaServices/Models/UserAvatarQueryResponse.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using JetBrains.Annotations;
 
 namespace Assets.Scripts
 {
@@ -13,11 +14,20 @@ namespace Assets.Scripts
 	//profiles?mode=lookup&avatar_username=
 	//Example: http://gaiaonline.com/profiles?mode=lookup&avatar_username=lm_a_kitty_cat
 	//<response status="200" avatarPath="ava/9f/8b/28c7853cf78b9f.png?t=1503043840_6.00_10" userId="16223135" />
+	/// <summary>
+	/// Response model sent back by one of the endpoints in <see cref="IGaiaOnlineQueryClient"/>.
+	/// Contains a manually encoded HTTP response code. This indicates the state of the model and the status of the response.
+	/// It does not match the actual HTTP response code sent back, which is always 200.
+	/// </summary>
 	[Serializable]
 	[XmlType("response")]
 	public sealed class UserAvatarQueryResponse
 	{
 		//TODO: Extract to interface
+		/// <summary>
+		/// The psuedo-response code for the response. Contains the actual response code
+		/// that will indicate success or failure. Actal response code is different and is always 200.
+		/// </summary>
 		[XmlAttribute("status")]
 		public int ResponseStatusCode { get; set; } //must be public setter for the XmlSerializer. May switch to DataContract instead.
 
@@ -25,18 +35,30 @@ namespace Assets.Scripts
 		[XmlIgnore]
 		public bool isRequestSuccessful => throw new NotImplementedException();
 
+		//This could be potentially null or empty if the request failed.
+		/// <summary>
+		/// The relative path to an avatar's URL.
+		/// Fits the format: ava/{a}/{b}/{c}.png?t={d}
+		/// d is currently unknown at this time.
+		/// </summary>
 		[XmlAttribute("avatarPath")]
 		public string AvatarRelativeUrlPath { get; set; } //must be public setter for the XmlSerializer. May switch to DataContract instead.
 
+		//This should be unique, though I can't prove that, and should
+		//also be possible to map to username or avatar path. No endpoint is known for that yet though.
 		//XML has issues with empty strings deserializing to ints, maybe only Mono? So we'll keep this as a string
 		//This will only ever be "" when an error occurs anyway
 		//TODO: Any use for this at the momemt?
+		/// <summary>
+		/// Unique UserId related to the Avatar.
+		/// </summary>
 		[XmlAttribute("userId")]
 		public string UserId { get; set; } //must be public setter for the XmlSerializer. May switch to DataContract instead.
 
-		public UserAvatarQueryResponse(int responseStatusCode, string avatarRelativeUrlPath, string userId)
+		public UserAvatarQueryResponse(int responseStatusCode, string avatarRelativeUrlPath, [NotNull] string userId)
 		{
 			if (string.IsNullOrWhiteSpace(avatarRelativeUrlPath)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(avatarRelativeUrlPath));
+			if (string.IsNullOrWhiteSpace(userId)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(userId));
 
 			ResponseStatusCode = responseStatusCode;
 			AvatarRelativeUrlPath = avatarRelativeUrlPath;
@@ -52,6 +74,7 @@ namespace Assets.Scripts
 		/// <inheritdoc />
 		public override string ToString()
 		{
+			//Just format it nicely.
 			return $"{nameof(ResponseStatusCode)}: {ResponseStatusCode} {nameof(AvatarRelativeUrlPath)}: {AvatarRelativeUrlPath} {nameof(UserId)}: {UserId}";
 		}
 	}
