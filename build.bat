ade06cb447d1857ad7a5da49a15b5453df211f52
HelloKitty <andrew.blakely@ymail.com>
2017-08-20 06:26:34 -0500

    Added Additional Dep project

--- build.bat
+++ build.bat
@@ -2,11 +2,13 @@ dotnet restore GaiaOnline.Unity.Library.sln
 dotnet publish src/GaiaOnline.Game.Client/GaiaOnline.Game.Client.csproj -c release
 dotnet publish src/GaiaOnline.Game.Common/GaiaOnline.Game.Common.csproj -c release
 dotnet publish src/GaiaOnline.Game.Network.Client/GaiaOnline.Game.Network.Client.csproj -c release
+dotnet publish src/GaiaOnline.AdditionalDependencies/GaiaOnline.AdditionalDependencies.csproj -c release
 
 if not exist "build" mkdir build
 if not exist "build\release" mkdir build\release
 xcopy src\GaiaOnline.Game.Client\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
 xcopy src\GaiaOnline.Game.Common\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
 xcopy src\GaiaOnline.Game.Network.Client\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
+xcopy src\GaiaOnline.AdditionalDependencies\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
 
 PAUSE
---
+++ src/GaiaOnline.Game.Client/InitializationEngine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaiaOnline
+{
+	public sealed class InitializationEngine
+	{
+		//TODO: Enable interface serialization
+	}
+}
