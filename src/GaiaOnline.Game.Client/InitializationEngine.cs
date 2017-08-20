04649be924e216cec9112eb2098bbcf2a7568f2e
HelloKitty <andrew.blakely@ymail.com>
2017-08-20 06:30:33 -0500

    Setup test InitializationEngine

--- src/GaiaOnline.Game.Client/InitializationEngine.cs
+++ src/GaiaOnline.Game.Client/InitializationEngine.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace GaiaOnline
 {
 	public sealed class InitializationEngine
 	{
-		//TODO: Enable interface serialization
+		/// <summary>
+		/// The list of <see cref="IInitializable"/> listeners.
+		/// </summary>
+		[SerializeField]
+		private List<IInitializable> InitializationList { get; }
+
+		//TODO: How should we allow configuration?
+		[SerializeField]
+		private bool initializeOnEnable;
+
+		private void OnEnable()
+		{
+			//TODO: What about reiniaitlaize?
+			if(initializeOnEnable)
+				foreach(IInitializable i in InitializationList)
+					i.Initialize();
+		}
 	}
 }
