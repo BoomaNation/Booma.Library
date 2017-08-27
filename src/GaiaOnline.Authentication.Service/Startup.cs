b745564bba02bf1a6ae8bb96ef2c058a0aa2057b
HelloKitty <andrew.blakely@ymail.com>
2017-08-27 05:28:05 -0500

    Added NameQueryDatabase and config to Auth service

--- src/GaiaOnline.Authentication.Service/Startup.cs
+++ src/GaiaOnline.Authentication.Service/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using TypeSafe.Http.Net;
 
 namespace GaiaOnline
@@ -38,6 +40,11 @@ namespace GaiaOnline
 				.Build();
 
 			services.AddSingleton(client);
+
+			IOptions<DatabaseConfigModel> dbConfig = services.GetDatabaseConfig();
+
+			services.AddDbContext<GaiaNameQueryDatabaseContext>(options => options.UseMySql(dbConfig.Value.ConnectionString));
+			services.AddTransient<IGaiaNameRepository, DbContextBasedGaiaNameRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
