d952439ad163d6caa30dad6a1ea280bef632dfb2
HelloKitty <andrew.blakely@ymail.com>
2017-08-28 03:02:13 -0500

    Updated TryEnterGameServer action

--- src/GaiaOnline.GameServerMediator.Service/Controllers/GatewayController.cs
+++ src/GaiaOnline.GameServerMediator.Service/Controllers/GatewayController.cs
@@ -17,25 +17,49 @@ namespace GaiaOnline
 		//TODO: When we switch to OAuth enable
 		//[Authorize]
 		[HttpPost("enter")]
-		public async Task<JsonResult> TryEnterGameServer()
+		public async Task<JsonResult> TryEnterGameServer([FromServices] IGameSessionRepository gameSessionRepository)
 		{
 			if(!ModelState.IsValid)
 				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.FailedConnectionActivelyRefused));
 
+			//TODO: Once this is OAuth2 we can't just parse the token like this
+			int? potentialId = TryGetUserId(Request);
+
+			if(!potentialId.HasValue)
+				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.FailedConnectionActivelyRefused));
+
+			int userId = potentialId.Value;
+	
+			bool hasSessionAlready = await gameSessionRepository.HasSession(userId);
+
+			if(hasSessionAlready)
+				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.FailedAlreadyLoggedIn));
+
+			//This could result in a data race so we need to check the result after we create. Don't assume this won't fail or be exploited
+			SessionCreationResult result = await gameSessionRepository.TryCreateSession(userId, Request.HttpContext.Connection.RemoteIpAddress.ToString());
+
+			if(!result.isSessionCreated)
+				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.GeneralServerError));
+
+			//TODO: Handle gameserver/zoneserver redirection based on session information.
+			return new JsonResult(new ServerEntryResponse(result.SessionGuid, new ResolvedEndpoint("127.0.0.1", 8051))); //TODO: Obviously we want to look in the DB and provide a real token.
+		}
+
+		public int? TryGetUserId(HttpRequest request)
+		{
 			//TODO: When we enable actual OAuth we won't need to manually read this
 			if(Request.Headers.ContainsKey("Authorization"))
-				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.FailedConnectionActivelyRefused));
+				return null;
 
 			string token = Request.Headers["Authorization"].FirstOrDefault();
 
-			if(token == null)
-				return new JsonResult(new ServerEntryResponse(ServerEntryResponseCode.FailedConnectionActivelyRefused));
+			if(String.IsNullOrWhiteSpace(token))
+				return null;
 
-			//TODO: In a real server we should check the database for if they are logged in
-			//TODO: If they are we should reject. Otherwise we need to find out where they are in the world
-			//TODO: Because if there are multiple zone servers, or instance servers, we need to create a session on for them on those
-			//TODO: And redirect them to the proper zone
-			return new JsonResult(new ServerEntryResponse(Guid.NewGuid(), new ResolvedEndpoint("127.0.0.1", 8051))); //TODO: Obviously we want to look in the DB and provide a real token.
+			int id;
+			int.TryParse(token, out id);
+
+			return id;
 		}
 	}
 }
---
+++ src/GaiaOnline.GameServerMediator.Service/Repository/IGameSessionRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GaiaOnline
+{
+	public interface IGameSessionRepository
+	{
+		/// <summary>
+		/// Indicates if the <see cref="userId"/> is associated with an active gamesession.
+		/// </summary>
+		/// <param name="userId">The user id to check.</param>
+		/// <returns>True if a game session exists for the user.</returns>
+		Task<bool> HasSession(int userId);
+
+		//TODO: The user should be authenticated, and with OAuth can prove it, so we just require id. Switch to OAuth ASAP
+		/// <summary>
+		/// Tries to create a gamesession for the user.
+		/// </summary>
+		/// <param name="userId"></param>
+		/// <param name="ipAddress"></param>
+		/// <returns></returns>
+		Task<SessionCreationResult> TryCreateSession(int userId, string ipAddress);
+	}
+}
