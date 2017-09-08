b681d84b023bf25b2ca4dc016c4b3e23aa7310fc
HelloKitty <andrew.blakely@ymail.com>
2017-09-08 05:39:27 -0500

    Convert Booma.Authentication.Client to Vs2017 format

--- build.bat
+++ build.bat
@@ -1,15 +1,14 @@
-dotnet restore GaiaOnline.Unity.Library.sln
-dotnet publish src/GaiaOnline.Game.Common/GaiaOnline.Game.Common.csproj -c release
-dotnet publish src/GaiaOnline.Game.Client/GaiaOnline.Game.Client.csproj -c release
-dotnet publish src/GaiaOnline.Game.Network.Client/GaiaOnline.Game.Network.Client.csproj -c release
-dotnet publish src/GaiaOnline.AdditionalDependencies/GaiaOnline.AdditionalDependencies.csproj -c release
+dotnet restore Booma.Library.sln
+dotnet publish src/Booma.Instance.Client/Booma.Instance.Client.csproj -c release
+dotnet publish src/Booma.Instance.Server/Booma.Instance.Server.csproj -c release
+dotnet publish src/Unity.Editor.Dependencies/Unity.Editor.Dependencies.csproj -c release
 
-if not exist "build" mkdir build
-if not exist "build\release" mkdir build\release
-xcopy lib\Booma.Client build\release /Y /EXCLUDE:BuildExclude.txt
-xcopy src\GaiaOnline.Game.Client\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
-xcopy src\GaiaOnline.Game.Common\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
-xcopy src\GaiaOnline.Game.Network.Client\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
-xcopy src\GaiaOnline.AdditionalDependencies\bin\Release\net46\publish build\release /Y /EXCLUDE:BuildExclude.txt
+if not exist "build\client\release" mkdir build\client\release
+if not exist "build\server\release" mkdir build\server\release
+xcopy src\Booma.Instance.Client\bin\Release\net46\publish build\client\release /Y /EXCLUDE:BuildExclude.txt
+xcopy src\Booma.Instance.Server\bin\Release\net46\publish build\server\release /Y /EXCLUDE:BuildExclude.txt
+
+xcopy src\Unity.Editor.Dependencies\bin\Release\net46\publish build\client\release /Y /EXCLUDE:BuildExclude.txt
+xcopy src\Unity.Editor.Dependencies\bin\Release\net46\publish build\server\release /Y /EXCLUDE:BuildExclude.txt
 
 PAUSE
--- src/Booma.Authentication.Client/Modules/AuthenticationServiceRegisterationModule.cs
+++ src/Booma.Authentication.Client/Modules/AuthenticationServiceRegisterationModule.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
