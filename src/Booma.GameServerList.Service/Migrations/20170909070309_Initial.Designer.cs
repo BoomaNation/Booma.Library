165db8509f5597dffe5e5d1bf97b68adbfab4c68
HelloKitty <andrew.blakely@ymail.com>
2017-09-09 02:03:42 -0500

    Added GameServer migrations

---
+++ src/Booma.GameServerList.Service/Migrations/20170909070309_Initial.Designer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Booma;
+using HaloLive.Network.Common;
+
+namespace Booma.GameServerList.Service.Migrations
+{
+    [DbContext(typeof(GameServerListDbContext))]
+    [Migration("20170909070309_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("Booma.GameServerListEntryModel", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("EndpointAddress")
+                        .IsRequired();
+
+                    b.Property<int>("EndpointPort");
+
+                    b.Property<int>("Region");
+
+                    b.Property<string>("ServerName")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("GameServers");
+                });
+        }
+    }
+}
---
+++ src/Booma.GameServerList.Service/Migrations/GameServerListDbContextModelSnapshot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Booma;
+using HaloLive.Network.Common;
+
+namespace Booma.GameServerList.Service.Migrations
+{
+    [DbContext(typeof(GameServerListDbContext))]
+    partial class GameServerListDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("Booma.GameServerListEntryModel", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("EndpointAddress")
+                        .IsRequired();
+
+                    b.Property<int>("EndpointPort");
+
+                    b.Property<int>("Region");
+
+                    b.Property<string>("ServerName")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("GameServers");
+                });
+        }
+    }
+}
