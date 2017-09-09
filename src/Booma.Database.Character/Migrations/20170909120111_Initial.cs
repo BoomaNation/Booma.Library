cd3deda671bfdb213241c6ff1b3a79bbba2cc86f
HelloKitty <andrew.blakely@ymail.com>
2017-09-09 07:02:57 -0500

    Added Character db migrations

---
+++ src/Booma.Database.Character/Migrations/20170909120111_Initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Booma.Database.Character.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "characters",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAdd", true),
+                    AccountId = table.Column<int>(nullable: false),
+                    CharacterName = table.Column<string>(maxLength: 20, nullable: false),
+                    CreationIp = table.Column<string>(maxLength: 15, nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGeneratedOnAddOrUpdate", true),
+                    isLoggedIn = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_characters", x => x.CharacterId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character_appearance",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(nullable: false),
+                    @class = table.Column<int>(name: "class", nullable: false),
+                    SectionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character_appearance", x => x.CharacterId);
+                    table.ForeignKey(
+                        name: "FK_character_appearance_characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "characters",
+                        principalColumn: "CharacterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "character_appearance");
+
+            migrationBuilder.DropTable(
+                name: "characters");
+        }
+    }
+}
---
+++ src/Booma.Database.Character/Migrations/CharacterDatabaseContextModelSnapshot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Booma;
+
+namespace Booma.Database.Character.Migrations
+{
+    [DbContext(typeof(CharacterDatabaseContext))]
+    partial class CharacterDatabaseContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1");
+
+            modelBuilder.Entity("Booma.CharacterAppearanceModel", b =>
+                {
+                    b.Property<int>("CharacterId");
+
+                    b.Property<int>("CharacterClass")
+                        .HasColumnName("class");
+
+                    b.Property<int>("SectionId");
+
+                    b.HasKey("CharacterId");
+
+                    b.ToTable("character_appearance");
+                });
+
+            modelBuilder.Entity("Booma.CharacterDatabaseModel", b =>
+                {
+                    b.Property<int>("CharacterId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AccountId");
+
+                    b.Property<string>("CharacterName")
+                        .IsRequired()
+                        .HasMaxLength(20);
+
+                    b.Property<string>("CreationIp")
+                        .IsRequired()
+                        .HasMaxLength(15);
+
+                    b.Property<DateTime>("CreationTime")
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<bool>("isLoggedIn");
+
+                    b.HasKey("CharacterId");
+
+                    b.ToTable("characters");
+                });
+
+            modelBuilder.Entity("Booma.CharacterAppearanceModel", b =>
+                {
+                    b.HasOne("Booma.CharacterDatabaseModel", "CharacterEntry")
+                        .WithMany()
+                        .HasForeignKey("CharacterId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
