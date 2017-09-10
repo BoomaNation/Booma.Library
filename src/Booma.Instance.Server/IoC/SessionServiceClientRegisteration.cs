e84c5e09d73c66d47622457b707d9862d75fad97
HelloKitty <andrew.blakely@ymail.com>
2017-09-10 01:05:14 -0500

    Added IGameServerSessionService registeration modules

---
+++ src/Booma.Instance.Server/IoC/SessionServiceClientRegisteration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+using TypeSafe.Http.Net;
+
+namespace Booma
+{
+	public sealed class SessionServiceClientRegisteration : NonBehaviourDependency
+	{
+		//TODO: Handle HTTPS cert issue
+		public override void Register(IServiceRegister register)
+		{
+			register.RegisterInstance(TypeSafeHttpBuilder<IGameServerSessionService>.Create()
+				.RegisterDefaultSerializers()
+				.RegisterJsonNetSerializer()
+				.RegisterDotNetHttpClient(@"http://localhost:5003")
+				.Build()); //TODO: How should an instance server find its mediator?
+		}
+	}
+}
